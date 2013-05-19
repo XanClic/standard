e4f189a5775a39a1ec782982300116dc3def145b
Alexander Kluth <derhartmut@niwohlos.org>
2013-05-19 21:40:27 +0200

    Added getopt_long for parsing cmdline options
    -h or --help and -v or --version
    
    Signed-off-by: Alexander Kluth <derhartmut@niwohlos.org>

--- src/main.c
+++ src/main.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <getopt.h>
 
 #include "buffer.h"
 #include "config.h"
@@ -6,8 +9,40 @@
 #include "term.h"
 
 
+static struct option long_options[] =
+{
+    {"help", no_argument, 0, 'h'},
+    {"version", no_argument, 0, 'v'},
+    {0, 0, 0, 0}
+};
+
 int main(int argc, char *argv[])
 {
+    int c;
+    int option_index = 0;
+
+    if (argc < 2)
+    {
+        fprintf(stderr, "Argument expected.\n");
+        return 1;
+    }
+
+    while ((c = getopt_long(argc, argv, "hv", long_options, &option_index)) != -1)
+    {
+        switch (c)
+        {
+            case 'h':
+                printf("Usage: std FILE\n"
+                       "--help, -h     Show this help\n"
+                       "--version, -v  Show current version\n");
+                return 0;
+            case 'v':
+                printf("standard: A fast and extensible terminal editor.\n");
+                return 0;
+            default:
+                return 0;
+        }
+    }
     load_config();
 
     term_init();
