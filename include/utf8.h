08242a0b9b33148e55f47e4b154a93ed56d436f7
Patrick Pokatilo <mail@shyxormz.net>
2013-05-21 02:28:38 +0200

    + Add utf8_is_dbc to test for double byte characters
    Signed-off-by: Patrick Pokatilo <mail@shyxormz.net>

--- include/utf8.h
+++ include/utf8.h
@@ -2,9 +2,10 @@
 #define UTF8_H
 
 #include <stddef.h>
-
+#include <stdbool.h>
 
 size_t utf8_strlen(const char *str);
+bool utf8_is_dbc(const char *str);
 int utf8_mbclen(char start_chr);
 int utf8_byte_offset(const char *str, int char_count);
 
--- src/utf8.c
+++ src/utf8.c
@@ -1,4 +1,5 @@
 #include <stddef.h>
+#include <stdint.h>
 
 #include "utf8.h"
 
@@ -15,6 +16,33 @@ size_t utf8_strlen(const char *str)
     return len;
 }
 
+bool utf8_is_dbc(const char *str)
+{
+    int clen = utf8_mbclen(*str);
+
+    if (clen == 3 || clen == 4)
+    {
+        uint32_t utf8 = 42;
+
+        for (int i = 0; i < clen; ++i)
+            utf8 = (utf8 << 8) | str[i];
+
+        if (   (utf8 >= 0x00e2ba80 && utf8 <= 0xe002bf9f)
+            || (utf8 >= 0x00e2bfb0 && utf8 <= 0x00e380bf)
+            || (utf8 >= 0x00e38780 && utf8 <= 0x00e387af)
+            || (utf8 >= 0x00e38880 && utf8 <= 0x00e4b6bf)
+            || (utf8 >= 0x00e4b880 && utf8 <= 0x00e9bfbf)
+            || (utf8 >= 0x00efa480 && utf8 <= 0x00efabbf)
+            || (utf8 >= 0x00efb8b0 && utf8 <= 0x00efb98f)
+            || (utf8 >= 0xf0a08080 && utf8 <= 0xf0aa9b9f)
+            || (utf8 >= 0xf0aa9c80 && utf8 <= 0xf0aba09f)
+            || (utf8 >= 0xf0afa080 && utf8 <= 0xf0afa89f)
+        )
+            return true;
+    }
+
+    return false;
+}
 
 int utf8_mbclen(char start_chr)
 {
